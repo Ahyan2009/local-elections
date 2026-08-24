b3187ecba6bf1fccc2837c480244f1d8f5622c4b
fouz ul Abad <fouzulabad.6@gmail.com>
2026-08-24 14:15:21 +0500

    fix: resolve axios.js merge conflict

--- frontend/Local-Elections/src/api/axios.js
+++ frontend/Local-Elections/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://candidate-system-api.ahyanmk3.workers.dev/api',
-=======
   baseURL: import.meta.env.VITE_API_URL || 'https://candidate-system-api.ahyanmk3.workers.dev/api',
->>>>>>> bfa7733bc1ca36379340e1bb17b4c79b26470854
 });
 
 export default API;
